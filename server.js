d06c06e2608680712d46f68ce70e9d5589072d06
retrofox <rdsuarez@gmail.com>
2018-07-20 10:34:00 -0500

    creting rest api proxy

--- server.js
+++ server.js
@@ -1,29 +1,56 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const Koa = require('koa');
+const path = require('path');
+const next = require('next');
+const bodyParser = require('koa-body');
+const Router = require('koa-router');
+const fetch = require('isomorphic-unfetch');
+
+const config = {
+	port: 3000,
+	host: 'localhost',
+}
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const defaultHandler = app.getRequestHandler()
 
-app.prepare().then( () => {
-	createServer( ( req, res ) => {
-		const parsedUrl = parse(req.url, true)
-		const { pathname, query } = parsedUrl;
-		
 
-		if ( pathname === '/a' ) {
-			app.render( req, res, '/b', query )
-		} else if ( pathname === '/b' ) {
-			app.render( req, res, '/a', query )
-		} else {
-			handle( req, res, parsedUrl );
-		}
-	} ).listen( 3000, err => {
+app.prepare().then(() => {
+	const server = new Koa();
+	const router = new Router();
+
+	server.proxy = true
+	server.use(bodyParser());
+
+	router.get('/api/:namespace/:version/', async context => {
+		const { res, params, query } = context;
+		console.log( {params} );
+		console.log( {query} );
+
+		// if (typeof handler === 'function') {
+		result = await fetch( `https://public-api.wordpress.com/${params.namespace}/${params.version}/` )
+			.then( resp => resp.json() )
+			.catch( err => err );
+
+		context.body = result;
+		context.respond = true
+	})
+
+	router.get('*', async context => {
+		const { req, res, params, query } = context
+		await defaultHandler( req, res );
+	})
+
+	server.use(async (context, next) => {
+		context.res.statusCode = 200;
+		await next();
+	})
+
+	server.use(router.routes())
+	server.listen( config.port, err => {
 		if (err) {
-			throw err;
+			throw err
 		}
-
-		console.log('> Ready on http://localhost:3000')
-	} )
-} )
+		console.log(`> Ready on ${config.host}:${config.port}`)
+	})
+});
